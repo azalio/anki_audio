ece1bae5744c3a7ce1a1fef273865ad59cb5a9c3
Mikhail Petrov <azalio@yandex-team.ru>
2016-11-06 19:25:01 +0300

    some improvements

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # anki_mp3
-Adding mp3 files to anki cards
+
+anki_mp3 позволяет к экпортируемому с сайта wordsfromtext.com
+списку слов для изучения добавить озвучку.
+
+Чтобы запустить программу необходимо:
+* Склонировать себе репозиторий:
+```bash
+git clone git@github.com:azalio/anki_mp3.git
+```
+* Перейти в папку репозитория
+```
+cd anki_mp3
+```
+и установить необходимые модули:
+```
+pip install -r requirements.txt
+```
+* Получить ключ [microsoft cognitive services (5000 озвучиваний
+в месяц бесплатно)](
+https://www.microsoft.com/cognitive-services/en-US/sign-up?ReturnUrl=/cognitive-services/en-us/subscriptions)
+* Переместить файл config.json.example в config.json
+и отредактировать его:
+```
+mv config.json.example config.json
+
+vim config.json
+```
+В нем надо указать оба ключа, которые вы получили шагом ранее.
+Так же необходимо указать путь к директории, где Anki [хранит
+медиа контент](http://ankisrs.net/docs/manual.html#file-locations).
+У меня он, например, находится по пути:
+```
+/Users/azalio/Documents/Anki/1-й пользователь/collection.media
+```
+* Скачать с сайта [wordsfromtext.com](http://wordsfromtext.com) файл со словами
+для изучения (anki с 1 предложением)
+* Запустить программу:
+```
+./anki_mp3 filename
+```
+где filename файл, который вы скачали на предыдущем шаге.
+* Программа будет пытаться скачать озвучку с сайта google и ms.
+* Если по каким-то причинам работа программы будет прервана,
+просто запустите ее заново.
+* В итоге получится файл с названием [название вашего файла].new
+Его и надо импортировать в anki
+
+
--- add_mp3.py
+++ add_mp3.py
@@ -15,14 +15,20 @@ def parse(line):
               r'([\w\s\.?\-?]+)<br/>'
     result = re.search(pattern, line)
     if result:
-        return result.group(1)
+        word = result.group(1)
+        if len(word.split()) > 1:
+            pattern = r'^to (\w+)$'
+            result = re.match(pattern, word)
+            if result:
+                word = result.group(1)
+        return word
     else:
         print('Error in regexp?\n{}'.format(line))
         return False
 
 
-def mp3_save(response, format):
-    with open(MEDIA_FOLDER + '/' + word + '.' + format, 'wb') as fh:
+def mp3_save(response, word, audio_format):
+    with open(MEDIA_FOLDER + '/' + word + '.' + audio_format, 'wb') as fh:
         for chunk in response:
             fh.write(chunk)
     return True
@@ -31,17 +37,20 @@ def mp3_save(response, format):
 def download_from_google(word):
     print('Try downloading from Google')
     if len(word.split()) > 1:
-        pattern = r'^to (\w+)$'
-        result = re.match(pattern, word)
-        if result:
-            word = result.group(1)
-        else:
-            return False
+        return False
     google_url = 'https://ssl.gstatic.com/dictionary/static/sounds/de/0/'
     url = google_url + word + '.mp3'
-    response = requests.get(url, stream=True)
+    try:
+        response = requests.get(url, stream=True)
+    except requests.ConnectionError as exp:
+        print('Some HTTP problems, {}'.format(exp))
+        return False
+    except requests.Timeout as exp:
+        print('HTTP timeout, {}'.format(exp))
+        return False
     if response.status_code == 200:
-        if mp3_save(response, 'mp3'):
+        if mp3_save(response, word, 'mp3'):
+            print("Download from Google was successful.\nSaving result.")
             return True
         else:
             print('Can\'t save file')
@@ -55,7 +64,14 @@ def ms_auth():
     ms_url_auth = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
     headers = {"Content-type": "application/x-www-form-urlencoded", "Content-Length": '0',
                "Ocp-Apim-Subscription-Key": MS_KEY}
-    response = requests.post(ms_url_auth, headers=headers)
+    try:
+        response = requests.post(ms_url_auth, headers=headers)
+    except requests.ConnectionError as exp:
+        print('Some HTTP problems, {}'.format(exp))
+        return False
+    except requests.Timeout as exp:
+        print('HTTP timeout, {}'.format(exp))
+        return False
     if response.status_code == 200:
         return response.content
     elif response.status_code == 429:
@@ -68,6 +84,7 @@ def ms_auth():
 
 
 def download_from_ms(word):
+    global MS_AUTH_TOKEN
     print('Try downloading from Microsoft')
 
     ms_auth_token = MS_AUTH_TOKEN
@@ -84,16 +101,24 @@ def download_from_ms(word):
                "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
                "User-Agent": "TTSForPython"}
 
-    response = requests.post(ms_url_tts, data=body, headers=headers)
+    try:
+        response = requests.post(ms_url_tts, data=body, headers=headers, stream=True)
+    except requests.ConnectionError as exp:
+        print('Some HTTP problems, {}'.format(exp))
+        return False
+    except requests.Timeout as exp:
+        print('HTTP timeout, {}'.format(exp))
+        return False
     if response.status_code == 200:
-        if mp3_save(response, 'wav'):
+        print("Download from MS was successful.\nSaving result.")
+        if mp3_save(response, word, 'wav'):
             return True
     elif response.status_code == 403:
         time.sleep(60)
         print('Get 403 from ms voice recognition, retry auth')
-        global MS_AUTH_TOKEN
         MS_AUTH_TOKEN = ms_auth()
-        download_mp3(word)
+        if MS_AUTH_TOKEN:
+            download_mp3(word)
     elif response.status_code == 503:
         print('Some problem in MS\nStatus code is {}'.format(response.status_code))
         return False
@@ -113,31 +138,49 @@ def download_mp3(word):
 def mp3(word):
     print('Check existing mp3s')
     if os.path.isfile(MEDIA_FOLDER + '/' + word + '.mp3'):
+        print('File exists')
         return 'mp3'
     elif os.path.isfile(MEDIA_FOLDER + '/' + word + '.wav'):
+        print('File exists')
         return 'wav'
     else:
         print('File not exists in library\nDownloading...')
-        audio_file = download_mp3(word)
-        if audio_file:
-            return audio_file
+        audio_file_extension = download_mp3(word)
+        if audio_file_extension:
+            return audio_file_extension
         else:
             return False
 
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
+    try:
+        filename = sys.argv[1]
+    except IndexError as exp:
+        print('Please, run program with argument:\n{} filename'.format(sys.argv[0]))
+        exit(1)
     ANKI_PATH = config.conf['anki']['path']
     MEDIA_FOLDER = ANKI_PATH + 'collection.media'
     MS_KEY = config.conf['ms']['ms_key1']
     MS_AUTH_TOKEN = ms_auth()
 
-    # TODO
-    # save file position if program was break and start with this position
-
-    with open(filename, 'r') as fh, open(filename + '.new', 'a') as new_fh:
+    with open(filename, 'r') as fh, open(filename + '.new', 'a') as new_fh, open(filename + '.seek', 'a+') as seek_fh:
+        seek_fh.seek(0)
+        position = seek_fh.readline()
+        try:
+            position = int(position)
+            fh.seek(position)
+            print('Move to new position {}'.format(position))
+        except ValueError as exp:
+            pass
         for line in fh:
             word = parse(line)
-            print('Word = {}'.format(word))
+            seek_fh.seek(0)
+            seek_fh.truncate()
+            seek_fh.write(str(fh.tell() - len(line)) + '\n')
+            print('============================\n{}'.format(word))
             extension = mp3(word)
-            new_fh.write('[sound:' + word + '.' + extension + ']' + line)
+            if extension:
+                new_fh.write('[sound:' + word + '.' + extension + ']' + line)
+                seek_fh.seek(0)
+                seek_fh.truncate()
+                seek_fh.write(str(fh.tell()) + '\n')
